3d64cb3a3ec09d6fefe707fd9d2b04067a5674b0
dongpd224 <dongpd224@gmail.com>
2021-11-21 23:07:19 +0700

    update laptop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
+// Scroll change background nav
 const nav = $('.main-navigation')
 window.addEventListener('scroll', e => {
     window.scrollY != 0 ? nav.classList.add('bg-white') : nav.classList.remove('bg-white')
